9a6419ab5724e2416d63537d4b93bc734521f91e
jusong23 <jusong23@naver.com>
2022-07-08 11:47:41 +0900

    Make SettingVC

---
+++ TimerZandi/SettingVC/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TimerZandi
+//
+//  Created by 이주송 on 2022/07/08.
+//
+
+import Foundation
---
+++ TimerZandi/SettingVC/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  TimerZandi
+//
+//  Created by 이주송 on 2022/07/08.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+}
