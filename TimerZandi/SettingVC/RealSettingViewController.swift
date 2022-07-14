e599cdccfa68725c24781937ab2ac925081bf9c9
jusong23 <jusong23@naver.com>
2022-07-14 21:11:41 +0900

    MyPage Set

---
+++ TimerZandi/SettingVC/RealSettingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RealSettingViewController.swift
+//  TimerZandi
+//
+//  Created by 이주송 on 2022/07/14.
+//
+
+import UIKit
+
+class RealSettingViewController: UIViewController {
+
+    @IBOutlet weak var dataPicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
--- TimerZandi/StaticVC/StaticViewController.swift
+++ TimerZandi/StaticVC/StaticViewController.swift
@@ -9,27 +9,11 @@ import UIKit
 
 class StaticViewController: UIViewController {
 
-    var testList:[String] = []
-    
-
-    func viewDate() -> Void {
-    let formatter_year = DateFormatter()
-    formatter_year.dateFormat = "mm분"
-    formatter_year.locale = Locale(identifier: "ko_KR")
-    let current_year_string = formatter_year.string(from: Date())
-// 앞에 값이랑 다를 때만 append
-        if self.testList.contains(current_year_string) == false {
-            self.testList.append((current_year_string))
-        }
-    }
-    
     @IBOutlet weak var zandiCollectionView: UICollectionView!
    
     @IBAction func reLoadButton(_ sender: UIButton) {
         debugPrint("새로고침합니다.")
         self.zandiCollectionView.reloadData()
-        self.viewDate()
-        debugPrint(testList)
     }
     
     override func viewDidLoad() {
@@ -40,7 +24,6 @@ class StaticViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("SeguePush VC 뷰가 나타날 것이다.")
-        AppearanceCheck(self)
     }
     
 
@@ -49,30 +32,6 @@ class StaticViewController: UIViewController {
         print("SeguePush VC 뷰가 나타났다.")
     }
     
-// var ZandiContents = [ZandiContents]()
-// 뷰 넘길때마다 합이 저장되어 색을 변경해야 하는데 셀 색깔 바꾸는 메소드가 한번밖에 안일어남. 한번에 저장한 다음에 콜렉션뷰로 리로드 해서 바꾸기
-    
-    //MARK: - 다크모드 변경함수 , 다른 VC의 함수 어떻게 호출?
-        
-        func AppearanceCheck(_ viewController: UIViewController) {
-            guard let appearance = UserDefaults.standard.string(forKey: "Appearance") else { return }
-            if appearance == "Dark" {
-                viewController.overrideUserInterfaceStyle = .dark
-                if #available(iOS 13.0, *) {
-                    UIApplication.shared.statusBarStyle = .lightContent
-                } else {
-                    UIApplication.shared.statusBarStyle = .default
-                }
-            } else {
-                viewController.overrideUserInterfaceStyle = .light
-                if #available(iOS 13.0, *) {
-                    UIApplication.shared.statusBarStyle = .darkContent
-                } else {
-                    UIApplication.shared.statusBarStyle = .default
-                }
-            }
-        }
-
     
     private func configureColletionView() {
             self.zandiCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
@@ -85,7 +44,7 @@ class StaticViewController: UIViewController {
 
 extension StaticViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 30, height: 30)
+        return CGSize(width: 35, height: 35)
 //        return CGSize(width: (UIScreen.main.bounds.width / 10), height: 40) // 행간의 Cell이 2개 표시됨
     } // sizeForItemAt 메소드 : 셀의 사이즈 찍어내는 메소드
 }
@@ -94,47 +53,18 @@ extension StaticViewController: UICollectionViewDataSource {
     // 아래는 필수 메소드 두개 !
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.testList.count
+        return 42
     } // 콜렉션뷰의 위치에 표시할 셀의 "갯수"[필수 메서드]
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ZandiCell", for: indexPath) as? ZandiCell else {return UICollectionViewCell() }
-        
-        let testData = UserDefaults.standard.integer(forKey: sumTime)
-
-        cell.cellTestLabel.text = self.testList[indexPath.row]
-
-        let formatter_year = DateFormatter()
-        formatter_year.dateFormat = "mm분"
-        formatter_year.locale = Locale(identifier: "ko_KR")
-        let current_year_string = formatter_year.string(from: Date())
-        
+        cell.cellTestLabel.text = String(indexPath.row + 1)
         cell.layer.cornerRadius = 6
         
-        if self.testList.contains(current_year_string) == false {
-            debugPrint("오늘이 아니다")
-        } else {
-            debugPrint("오늘이다 ")
-            
-            if testData > 4 && testData <= 9 && self.testList[indexPath.row] == current_year_string {
-                cell.backgroundColor = UIColor(red: 58/225, green: 211/225, blue: 83/225, alpha: 1.0)
-            } else if testData > 9 && testData <= 15 && self.testList[indexPath.row] == current_year_string {
-                cell.backgroundColor = UIColor(red: 38/225, green: 166/225, blue: 65/225, alpha: 1.0)
-            } else if testData > 15 && testData <= 20 && self.testList[indexPath.row] == current_year_string {
-                cell.backgroundColor = UIColor(red: 1/225, green: 109/225, blue: 50/225, alpha: 1.0)
-            } else if testData > 20 && testData <= 25 && self.testList[indexPath.row] == current_year_string {
-                cell.backgroundColor = UIColor(red: 13/225, green: 68/225, blue: 41/225, alpha: 1.0)
-            } else if testData < 3 {
-                cell.backgroundColor = .darkGray
-            }
-        }
+
 // 일자 달라지면 셀이 추가 되도록 ! 하지만 추가한 셀만 색변경이 적용되게는 아직 ..
 // 지금 날짜랑 일치한 셀만 색 변경가능하게
 // 분에 따라 각 셀의 색을 변화시켰지만, 하나의 키로 저장된 유저디폴트로만 이용하기에 다음 날이 되었을 때 새로고침하면 집중시간에 따른 색이 제대로 출력되지 않음
-        
-        debugPrint("콜렉션 뷰다 !!")
-
-         
         return cell
     } // 콜렉션뷰의 위치에 표시할 셀을 "요청"하는 메소드[필수 메서드]
     
