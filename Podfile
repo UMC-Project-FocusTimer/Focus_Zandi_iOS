af1a659a7ae2ca69f1a130268f13b935ebcf94b9
jusong23 <jusong23@naver.com>
2022-07-15 15:36:35 +0900

    FSCalendar

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'TimerZandi' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 pod ‘Alamofire’
+pod 'FSCalendar'
   # Pods for TimerZandi
 
 end
--- TimerZandi/SettingVC/RealSettingViewController.swift
+++ TimerZandi/SettingVC/RealSettingViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import FSCalendar
 
 class RealSettingViewController: UIViewController {
 
-    @IBOutlet weak var dataPicker: UIDatePicker!
+    @IBOutlet weak var calendar: FSCalendar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.calendarColor()
+        self.languageSet()
     }
     
+    func calendarColor() {
+        // 달력의 평일 날짜 색깔
+        calendar.appearance.titleDefaultColor = .black
+
+        // 달력의 토,일 날짜 색깔
+        calendar.appearance.titleWeekendColor = .red
+
+        // 달력의 맨 위의 년도, 월의 색깔
+        calendar.appearance.headerTitleColor = .systemPink
+
+        // 달력의 요일 글자 색깔
+        calendar.appearance.weekdayTextColor = .orange
+    }
     
+    func languageSet() {
+        // 달력의 년월 글자 바꾸기
+        calendar.appearance.headerDateFormat = "YYYY년 M월"
+
+        // 달력의 요일 글자 바꾸는 방법 1
+        calendar.locale = Locale(identifier: "ko_KR")
+                
+        // 달력의 요일 글자 바꾸는 방법 2
+        calendar.calendarWeekdayView.weekdayLabels[0].text = "일"
+        calendar.calendarWeekdayView.weekdayLabels[1].text = "월"
+        calendar.calendarWeekdayView.weekdayLabels[2].text = "화"
+        calendar.calendarWeekdayView.weekdayLabels[3].text = "수"
+        calendar.calendarWeekdayView.weekdayLabels[4].text = "목"
+        calendar.calendarWeekdayView.weekdayLabels[5].text = "금"
+        calendar.calendarWeekdayView.weekdayLabels[6].text = "토"
+    }
+
 }
